cd78d69fba518c802aad3fdaab071b4aeda164e4
Adam Brewer <ambreweruni@gmail.com>
2019-07-14 20:30:17 +0100

    beginings of testing

--- get_iplayer_python/ffmpeg_wrapper.py
+++ get_iplayer_python/ffmpeg_wrapper.py
@@ -14,3 +14,16 @@ def merge_audio_and_video(audio_file_location, video_file_location, output_file_
     except OSError as e:
         logger.exception(e.strerror)
     logger.debug(f"merged audio: {audio_file_location} video: {video_file_location} to {output_file_name}")
+
+
+def save_audio(audio_file_location, output_file_name):
+    logger = logging.getLogger(__name__)
+    logger.debug(f"saving audio: {audio_file_location} to {output_file_name}")
+    try:
+        subprocess.call(
+            ["ffmpeg",
+             "-i", audio_file_location,
+             output_file_name])
+    except OSError as e:
+        logger.exception(e.strerror)
+    logger.debug(f"saved audio: {audio_file_location} to {output_file_name}")
